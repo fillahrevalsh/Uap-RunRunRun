fb29d3aa51b886c359a2dff0e4a508ecec05c7ac
fillahrevalsh <fillahreval098@gmail.com>
2025-11-27 21:35:35 +0700

    capana

---
+++ Openingcapana.cpp
@@ -0,0 +1,10 @@
+Capana buat opening program nya ya..
+
+kaya materi awal ncurses kita yang nulis text bisa diatur dikordinat mana aja
+yang kalo kita pencet q program nya keluar.
+
+tapi buatnya dibagus bagusin 
+ada nama npm kita terus warna warni juduk projek.. oke?
+
+
+jadi buatnya pake itu.. kalo bingung ntar kuajari  di kampus
